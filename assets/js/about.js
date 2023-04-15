0a8d4d9223e8f99590ff69a49fb9ee8432fc339b
angelica-hom <90013439+angelica-hom@users.noreply.github.com>
2023-04-15 18:10:01 -0400

    Fixed nav bar spacing, fixed intro text formatting, cleaned up files

---
+++ assets/js/about.js
@@ -0,0 +1,5 @@
+document.addEventListener('keydown', function (event) {
+    if (event.code === 'ArrowLeft') {
+        window.location.href = 'conclusion.html';
+    }
+});
---
+++ assets/js/structure.js
@@ -0,0 +1,11 @@
+document.addEventListener('keydown', function (event) {
+    if (event.code === 'ArrowLeft') {
+        window.location.href = 'themes.html';
+    }
+});
+
+document.addEventListener('keydown', function (event) {
+    if (event.code === 'ArrowRight') {
+        window.location.href = 'universes.html';
+    }
+});
